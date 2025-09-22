1702da38a6f85dbecfe33294760ef81cd111776a
Sarang <149196971+sarang-j-lab@users.noreply.github.com>
2025-09-22 11:47:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const squares = document.querySelectorAll(".square");
+let dontPaint = null;
+
+squares.forEach((ele) => {
+    ele.addEventListener('mousemove', (e) => {
+        dontPaint = e.target.id
+        squares.forEach((element) => {
+            if (!(element.id == dontPaint)) {
+                element.style.backgroundColor = "#6F4E37";
+            }
+        })
+    })
+})
+
+squares.forEach((ele)=>{
+    ele.addEventListener("mouseout",(e)=>{
+        squares.forEach((ele)=>{
+            ele.style.backgroundColor = "lavender"
+        })
+    })
+})
